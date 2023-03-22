6f8b9ad5bdc5c6043422c5eabe1db94ef3361c92
Vinicius de Morais <viniciuz.lol5@gmail.com>
2023-03-21 22:25:12 -0300

    created documentation

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,15 @@ import express from "express";
 
 import { usersRoutes } from "./routes/users.routes";
 
+import swaggerUi from 'swagger-ui-express';
+
 const app = express();
+const swaggerDocument = require('./swagger.json');
+
 
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/users", usersRoutes);
 
